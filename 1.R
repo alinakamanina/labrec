9e3e17c949a3cf0e3286502a363a4ceb43234ce7
Alina Kamanina <kamanina.a@gmail.com>
2017-02-18 13:15:34 +0300

    тут ссылка

--- 1.R
+++ 1.R
@@ -1,13 +1,2 @@
 #####
-я делаю исправление
-<<<<<<< HEAD
-и я тоже
-
-whats going on
-=======
-ghdghdg
-прив
-<<<<<<< HEAD
-=======
->>>>>>> fc1854dcc7024dce8056f25078d7cc8db418d379
->>>>>>> a7b1574e6b75dbdfa9e94ab6769800e0752f4458
+https://www.r-bloggers.com/rstudio-and-github/
