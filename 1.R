b531c8c70e46b8739902098fefefe010fcac43b9
Alina Kamanina <kamanina.a@gmail.com>
2017-02-18 13:19:25 +0300

    link

--- 1.R
+++ 1.R
@@ -1,6 +1,6 @@
 #####
 
-https://www.r-bloggers.com/rstudio-and-github/ here is the answwer
+https://www.r-bloggers.com/rstudio-and-github/ here is the answer
 
 я делаю исправление
 всё стало хорошо?
